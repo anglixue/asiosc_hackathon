147b653c80f7aaec7fba1374e8001e7d4f09fd6d
anglixue <alexxuexue@gmail.com>
2022-08-22 13:44:19 +1000

    Add data exploreation script

---
+++ data_exploration.R
@@ -0,0 +1,12 @@
+##############################################################################
+# Script information
+# Title: Explore the data from ASI/OSC Hackathon
+# Author: Angli Xue
+# Date: 2022-08-22
+# Description: This R script was to explore the data provided by ASI/OSC Hackathon.
+##############################################################################
+
+
+
+
+####
