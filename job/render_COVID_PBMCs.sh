47b07dc367db0e17501ad101a9f00f22ba309fba
angxue <angxue@beefy-4-4.local>
2022-09-01 12:44:06 +1000

    Add qsub job file

---
+++ job/render_COVID_PBMCs.sh
@@ -0,0 +1,18 @@
+## SGE SETTINGS
+#$ -cwd
+#$ -S /bin/bash
+#$ -q short.q
+#$ -pe smp 1
+#$ -r yes
+#$ -l mem_requested=200G
+#$ -N render_COVID_PBMCs
+#$ -o stdout_render_COVID_PBMCs
+#$ -e stderr_render_COVID_PBMCs
+
+cd $SGE_O_WORKDIR
+
+Rscript=/share/ScratchGeneral/angxue/software/miniconda3/envs/py37/bin/Rscript
+
+$Rscript -e 'library(rmarkdown); rmarkdown::render("Azimuth_annotation_COVID_PBMCs.Rmd", "html_document")'
+
+
